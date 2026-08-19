c07e20fbc38d9560fea4f1942a57d7c10cdd6c00
Milenko Mitrovic <subzone@users.noreply.github.com>
2026-08-19 22:18:47 +0200

    Implement Redis connection and add unit tests

--- tests/test_redis_tools.py
+++ tests/test_redis_tools.py
@@ -4,3 +4,6 @@ from src.tools.redis_tools import redis_client
 @pytest.fixture
 def redis_client_fixture():
     return redis_client
+
+def test_redis_connection(redis_client_fixture):
+    assert redis_client_fixture.ping() == True
